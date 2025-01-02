a279ca1cf554d0e8821ec86e5d3ea177c46356c9
anasachu2000 <muhammedanasea2000@gmail.com>
2025-01-02 14:17:37 +0530

    last

--- src/components/home/common/Banner.tsx
+++ src/components/home/common/Banner.tsx
@@ -1,3 +1,5 @@
+import vedioBanner from "../../../../public/3761461-uhd_3840_2160_25fps.mp4"
+
 const Banner = ({ image }: { image: string }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-10">
@@ -13,7 +15,7 @@ const Banner = ({ image }: { image: string }) => {
       {/* Video Container */}
       <div className="h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[75vh] w-full">
         <video
-          src="../../public/3761461-uhd_3840_2160_25fps.mp4"
+          src={vedioBanner}
           autoPlay
           muted
           loop
