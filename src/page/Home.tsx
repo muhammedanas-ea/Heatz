6494a5f68e56362bdadf3d34bd8695626a5686cf
anasachu2000 <muhammedanasea2000@gmail.com>
2025-01-02 13:57:15 +0530

    last

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,6 +3,13 @@ import HeroCarousel from "../components/home/common/HeroCarousel ";
 import Navbar from "../components/navbar/Navbar";
 import ProductCarousel from "../components/home/ProductCarousel";
 import Banner from "../components/home/common/Banner";
+import bannerVedio from "../../public/3761461-uhd_3840_2160_25fps.mp4"
+import headphoneImge from "../../public/png-clipart-headphones-headphones__1_-removebg-preview.png"
+import Banner1 from "../../public/Screenshot (134).jpg"
+import Banner2 from "../../public/Screenshot (138).png"
+import Banner2 from "../../public/Screenshot (138).png"
+import Banner3 from "../../public/Screenshot (139).jpg"
+import Banner4 from "../../public/Screenshot (140).png"
 
 const products = [
   {
@@ -65,7 +72,7 @@ function Home() {
       <div className="relative h-[117vh] w-full bg-black">
         <div className="absolute inset-0">
           <video
-            src="../../public/3761461-uhd_3840_2160_25fps.mp4"
+            src={bannerVedio}
             autoPlay
             muted
             loop
@@ -82,17 +89,13 @@ function Home() {
         <div className="absolute inset-0 flex items-end justify-center px-4 md:px-6 ">
           <div className="max-w-6xl w-full mx-auto flex flex-wrap items-center justify-between p-4 bg-neutral-600/15 backdrop-blur-sm rounded-full border border-[#2e2d2d85]">
             <div className="flex space-x-4 gap-6 hide-scrollbar overflow-x-auto scroll-smooth snap-x snap-mandatory w-full">
-              {Array(14) // Replace with dynamic rendering if needed
-                .fill(
-                  "../../../public/png-clipart-headphones-headphones__1_-removebg-preview.png"
-                )
-                .map((src, index) => (
+              {Array(14).map((_,index) => (
                   <div
                     key={index}
                     className="w-14 h-14 flex-shrink-0 snap-center"
                   >
                     <img
-                      src={src}
+                      src={headphoneImge}
                       alt={`Headphone ${index + 1}`}
                       className="w-full h-full object-cover"
                     />
@@ -110,10 +113,10 @@ function Home() {
         />
         <HeroCarousel
           slides={slides}
-          image="../../public/Screenshot (134).jpg"
+          image={Banner1}
         />
         <ProductCarousel />
-        <Banner image={"../../public/Screenshot (138).png"} />
+        <Banner image={Banner2} />
         <div className="py-20">
           <FeatureHighlight
             title="Boost up your Gaming experience."
@@ -122,10 +125,10 @@ function Home() {
           />
           <HeroCarousel
             slides={slides}
-            image="../../public/Screenshot (139).jpg"
+            image={Banner3}
           />
           <ProductCarousel />
-          <Banner image={"../../public/Screenshot (140).png"} />
+          <Banner image={Banner4} />
         </div>
         <FeatureHighlight
           title="Trending Products."
@@ -138,7 +141,7 @@ function Home() {
           <div className="bg-gray-100 flex flex-col relative flex-shrink-0 w-[14rem]">
             <div className="flex justify-center  h-40 sm:h-48 md:h-80">
               <video
-                src="../../public/3761461-uhd_3840_2160_25fps.mp4"
+                src={bannerVedio}
                 autoPlay
                 muted
                 loop
